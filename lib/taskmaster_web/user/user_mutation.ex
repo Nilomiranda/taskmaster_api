3361fbceba639b6ebe9a36f0651741ff9329e9aa
Nilomiranda <nilomiranda3@gmail.com>
2021-12-15 22:50:35 -0300

    defined fields for create account mutation

--- lib/taskmaster_web/user/user_mutation.ex
+++ lib/taskmaster_web/user/user_mutation.ex
@@ -7,6 +7,10 @@ defmodule TaskmasterWeb.Mutations.User do
     @desc "Create new account"
 
     field :create_account, non_null(:user) do
+      arg :email, non_null(:string)
+      arg :name, non_null(:string)
+      arg :password, non_null(:string)
+
       resolve &UserResolver.create_user/3
     end
   end
