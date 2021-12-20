d1183797fba5f519e359ffbff43fc19f3b3d816c
Nilomiranda <nilomiranda3@gmail.com>
2021-12-20 19:53:29 -0300

    hashed user password

--- lib/taskmaster/accounts/user.ex
+++ lib/taskmaster/accounts/user.ex
@@ -1,6 +1,7 @@
 defmodule Taskmaster.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -8,14 +9,21 @@ defmodule Taskmaster.Accounts.User do
     field :email, :string
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
 
-  @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash])
-    |> validate_required([:name, :email, :password_hash])
+    |> cast(attrs, [:name, :email, :password])
+    |> create_hashed_password()
+    |> validate_required([:name, :email, :password])
   end
+
+  defp create_hashed_password(%Changeset{valid?: true, changes: %{ password: password } } = user) do
+    change(user, password_hash: Bcrypt.Base.hash_password(password, Bcrypt.gen_salt(12)))
+  end
+
+  defp create_hashed_password(changeset), do: changeset
 end
--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Taskmaster.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.6"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
